3c4fd8cea7336e5302d118a4831e6ac473ecc9e6
naya-h2 <hw7666@naver.com>
2025-04-06 02:49:47 +0900

    특정 date에 등록된 공지 필터링 함수 추가

--- cse/main.py
+++ cse/main.py
@@ -5,7 +5,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from enum import Enum
-from crawling import crawling_notices
+from crawling.crawling_first import crawling_notices
+from crawling.crawling_today import filter_date_notices
 
 class Notice_Type(str, Enum):
   important = "important"
@@ -26,8 +27,7 @@ def cse_today_notices(type: Notice_Type):
   notices = crawling_notices(CSE_Link[type], "/html/body/div/div[4]/div[2]/div[4]/div/div/ul", "li", "/div/div[2]/a", "/div/div[2]/div/span[2]")
   
   # TODO: 오늘 등록된 것만 거르는 로직 추가
-  
-  return notices
+  return filter_date_notices(notices, '2025.03.28')
 
 
-print(cse_today_notices(Notice_Type.job))
+# print(cse_today_notices(Notice_Type.job))
