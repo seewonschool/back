f16954af3a07c023200b943af06224749c4e8198
naya-h2 <hw7666@naver.com>
2025-04-05 14:51:28 +0900

    국어국문 공지 추가

---
+++ kor/main.py
@@ -0,0 +1,87 @@
+### 컴퓨터공학과 - 주요공지
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+from fake_useragent import UserAgent
+import datetime
+import time
+
+from dotenv import load_dotenv
+
+from cse.crawl_notices import crawl_today_notices
+
+def kor_important():
+    detecting_website = (
+        "https://korea.sogang.ac.kr/front/cmsboardlist.do?siteId=korea&bbsConfigFK=506"
+    )
+    detecting_interval = 60
+
+    title_parent = f"/html/body/div/div[4]/div[2]/div[3]/div/div/ul"
+
+
+    load_dotenv()
+
+
+    # Selenium options
+    chrome_options = webdriver.ChromeOptions()
+    ua = UserAgent()
+    userAgent = ua.random
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument(f"user-agent={userAgent}")
+
+    # 드라이버 실행
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()), 
+        options=chrome_options
+    )
+
+    # 3초 기다림
+
+    driver.implicitly_wait(10)
+    driver.get(detecting_website)
+    time.sleep(5)
+
+    el_notice_box = driver.find_element(By.XPATH, title_parent)
+    list_items = el_notice_box.find_elements(By.TAG_NAME, "li")
+    notice_num = len(list_items)
+
+    xpath_list = []
+    for i in range(1, notice_num):
+        notice = {
+            "title": f"/html/body/div/div[4]/div[2]/div[3]/div/div/ul/li[{i}]/div/div[2]/a",
+            "registered_date": f"/html/body/div/div[4]/div[2]/div[3]/div/div/ul/li[{i}]/div/div[2]/div/span[2]"
+        }
+        xpath_list.append(notice)
+
+    data = crawl_today_notices(driver, xpath_list)
+    print(data)
+    return data
+
+
+# old_notices = new_notices.copy()
+
+# try:
+#     # slack_api.notify_started(slack_channel)
+
+#     # 반복 실행
+#     while True:
+#         # 오늘 올라온 공지 detect
+#         crawl_today_notices()
+
+#         # 기존 공지-새 공지 비교
+#         detect_changed_notices()
+
+#         # 저장하고 있던 공지 업데이트
+#         old_notices = new_notices.copy()
+
+#         # 1분 후 다시 실행
+#         time.sleep(detecting_interval)
+#         driver.refresh()
+
+# finally:
+#     driver.quit()
+#     # slack_api.notify_terminated(slack_channel)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 
 from model.major import Major
 from cse.important import cse_important
+from kor.main import kor_important
 
 @app.get("/")
 def read_root():
@@ -20,3 +21,5 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def read_notice(major: Major):
     if major == "cse":
       return cse_important()
+    if major == "kor":
+        return kor_important()
