fa1179d7574acecdcc00ebe9a5a76a69ff823357
naya-h2 <hw7666@naver.com>
2025-06-02 21:56:04 +0900

    feat: firebase 연동 및 셀레니움으로 카카오 유저 등록 완료

--- login.py
+++ login.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
 load_dotenv()
 
@@ -40,34 +43,93 @@ login_button = driver.find_element(By.ID, 'btn-login')
 login_button.click()
 
 # 페이지 로딩을 기다림 (필요 시 시간 조정)
-driver.implicitly_wait(30)
-
-# 로그인 후 작업 수행 가능
-# 예: 데이터를 스크래핑하거나 특정 페이지로 이동
+driver.implicitly_wait(50)
 
 # 멤버 관리 버튼
 btn2 = driver.find_element(By.XPATH, '/html/body/div/main/section/div[1]/nav/ul/li[2]/strong')
 btn2.click()
 driver.implicitly_wait(30)
 
+##### 멤버 정보 등록
+# firebase 계정 키 파일 경로
+cred = credentials.Certificate("firebasekey.json")
+firebase_admin.initialize_app(cred)
+# Firestore 클라이언트 초기화
+db = firestore.client()
+students_ref = db.collection('students')
+docs = students_ref.stream()
 
-# 멤버 일괄 등록 버튼
-btn3 = driver.find_element(By.XPATH, '/html/body/div/main/section/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/button[2]')
-btn3.click()
-driver.implicitly_wait(5000)
+# 등록된 "~대학" 의 +버튼 path 전부 추가 (전부 열고 선택하는 방식으로)
+parent_major_path = ["/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[2]/div/div/button",
+                     "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[6]/div/div/button"]
 
-# 파일 업로드 input 요소 찾기
-file_path = os.path.abspath("students_data.xlsx")  # 업로드할 파일의 절대 경로
-driver.find_element(By.CSS_SELECTOR, "input[type='file']").send_keys(file_path)
-driver.implicitly_wait(5000)
+# 과별 버튼 path
+child_major_path = {
+    "컴퓨터공학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[3]",
+    "인공지능학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[4]/div/div/div/div/div[4]/div/div[2]/span/div",
+    "AI기반 자유전공학부": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[5]/div/div/div/div/div[4]/div/div[2]/span/div",
+    "전자공학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[7]/div/div/div/div/div[4]/div/div[2]/span/div",
+    "화공생명공학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[8]/div/div/div/div/div[4]/div/div[2]/span/div",
+    "기계공학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[9]/div/div/div/div/div[4]/div/div[2]/span/div",
+    "시스템반도체공학": "/html/body/div[5]/div/div/section/div[2]/div[1]/div/div/div/div/div[10]/div/div/div/div/div[4]/div/div[2]/span/div"
+}
 
-# 파일 등록 버튼
-btn5 = driver.find_element(By.XPATH, '/html/body/div[4]/div/div/section/div[3]/button')
-btn5.click()
-driver.implicitly_wait(5000)
-# 이후 작업 수행 가능
+for doc in docs :
+    # 멤버 등록 버튼
+    btn3 = driver.find_element(By.XPATH, '/html/body/div/main/section/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/button[3]')
+    btn3.click()
+    driver.implicitly_wait(30)
 
-input("종료금지")
+    row = {"id": doc.id, **doc.to_dict()}
+    # id(이메일)
+    driver.find_element(By.XPATH, '/html/body/div[4]/div/div/section/form/div[1]/fieldset[1]/div[1]/div/div/div/input').send_keys(row.get("email", ""))
+
+    # 이름
+    driver.find_element(By.XPATH, '/html/body/div[4]/div/div/section/form/div[1]/fieldset[1]/div[3]/div/div/input').send_keys(row.get("name", ""))
+
+    # 소속 버튼
+    major_btn1 = driver.find_element(By.XPATH, '/html/body/div[4]/div/div/section/form/div[1]/fieldset[2]/div[2]/div/div/div/button')
+    major_btn1.click()
+    major_btn1.click() #안눌려서 일부러 2번 넣은거임
+    driver.implicitly_wait(50)
+
+    # 조직 선택 버튼
+    major_btn2 = driver.find_element(By.XPATH, '/html/body/div[4]/div/div/section/form/div[1]/fieldset[2]/div[2]/div/div/div[1]/div[1]/div[2]')
+    major_btn2.click()
+    driver.implicitly_wait(70)
+
+    # 대학 버튼 전부 열기
+    for path in parent_major_path:
+        driver.find_element(By.XPATH, path).click()
+        driver.implicitly_wait(100)
+
+    # 유저 소속 버튼
+    user_major = (row.get("major")).split("|")
+    # print(user_major)
+
+    # print(child_major_path.get(user_major[2]))
+    major_btn = driver.find_element(By.XPATH, child_major_path.get(user_major[2]))
+    major_btn.click()
+    driver.implicitly_wait(50)
+
+    # # 소속 등록 확인 버튼
+    major_ok = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/section/div[3]/button[2]')
+    major_ok.click()
+    driver.implicitly_wait(70)
+
+    # input("되나")
+
+    #등록 버튼
+    driver.find_element(By.XPATH, '/html/body/div[3]/div/div/section/form/div[2]/button[2]').click()
+
+    #등록 완료 팝업 확인 버튼
+    driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div[3]/button').click()
+
+    # 등록한 유저 정보 firebase에서 삭제
+    doc.reference.delete()
+
+
+# input("종료금지")
 
 # 작업이 끝나면 브라우저 닫기
 driver.quit()
--- main.py
+++ main.py
@@ -3,103 +3,15 @@ from fastapi.responses import FileResponse
 from typing import Union
 from pydantic import BaseModel
 
-from openpyxl import Workbook
-from openpyxl.styles import Font
-from openpyxl import load_workbook
 from const.kakao_conversation import KakaoConversaionId, MajorCode
 from model.major import Major
 import requests
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-from datetime import datetime
-import os
-
 app = FastAPI()
-
-# firebase 계정 키 파일 경로
-cred = credentials.Certificate("firebasekey.json")
-firebase_admin.initialize_app(cred)
-# Firestore 클라이언트 초기화
-db = firestore.client()
 
 headers = {
         'Authorization': 'Bearer e0530d2e.12f54b7fdb1f4628a6280c34eee7ef0c',
         'Content-Type': 'application/json'
 }
-
-### 문서 데이터 추출 -> 엑셀을 만들어서 -> 삭제 -> 등록 -> 초대
-
-@app.get("/users/firebase")
-def get_students_collection():
-    # 'students' 컬렉션의 모든 문서 가져오기
-    students_ref = db.collection('students')
-    docs = students_ref.stream()
-
-    # 2. 엑셀 파일 생성
-    wb = Workbook()  # 새 엑셀 파일 생성
-    wb._write_version = "2210"                 # fileVersion.appName 대신 엑셀 버전
-    ws = wb.active   # 기본 워크시트 선택
-    # ws.title = "Students Data"  # 워크시트 이름 설정
-    # 메타데이터 설정
-    wb.properties.title = "Example Excel File"
-    wb.properties.creator = "My Application"
-    wb.properties.description = "This is a test Excel file created with Python."
-    wb.properties.lastModifiedBy = "AutomationBot"
-    wb.properties.created        = datetime.utcnow()
-    wb.properties.modified       = datetime.utcnow()
-    ws.title = "Sheet1"                        # ① 샘플과 동일한 시트명
-    ws.sheet_view.tabSelected = True
-    
-
-    # 데이터 헤더 추가
-    headers = [ "ID(이메일)*",
-                "이름*",
-                "닉네임",
-                "조직",
-                "직책",
-                "직위",
-                "휴대전화",
-                "유선전화",
-                "이메일",
-                "근무위치",
-                "생일",
-                "상태 메세지"]  # 각 필드에 맞는 헤더 설정
-    ws.append(headers)  # 첫 번째 행에 헤더 추가
-
-    for doc in docs :
-      row = {"id": doc.id, **doc.to_dict()}
-      # 엑셀에 데이터 추가
-      ws.append([row.get("email", ""), row.get("name", ""), "", row.get("major", ""),"", "","","",row.get("email", ""),"", "", ""])
-
-    
-    # 엑셀파일 폰트 설정 ...
-    font_style = Font(name="Malgun Gothic", size=11)
-    for row in ws.iter_rows():  # 시트의 모든 행을 순회
-        for cell in row:  # 해당 행의 모든 셀을 순회
-            cell.font = font_style
-
-    # 3. 파일 저장 경로 설정
-    file_name = "students_data"
-    wb.save(file_name)  # 엑셀 파일 저장
-
-    # 4. firebase 내 데이터 삭제
-    students_ref_re = db.collection('students') #-> 중간에 들어온 사람은 엑셀 못들어가고 삭제될듯..ㅎ?
-    docs_re = students_ref_re.stream()
-    for doc in docs_re:
-        doc.reference.delete()
-    
-    # 5. 파일 응답 반환
-    # 파일이 존재하는지 확인
-    if not os.path.exists(file_name):
-        return {"error": "File not found!"}
-    
-    # 올바른 파일 경로로 반환
-    return FileResponse(
-        path=file_name,
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        filename=file_name
-    )
 
 @app.get("/users/kakao")
 def get_kakao_users():
